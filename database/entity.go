767cd1a91d1d98ba1f18e424fbb41c72416ff9b9
jasonkhew96 <29531167+JasonKhew96@users.noreply.github.com>
2021-10-24 20:30:33 +0800

    Add column name

--- database/entity.go
+++ database/entity.go
@@ -28,48 +28,48 @@ const (
 // AccessKeys key 缓存
 type AccessKeys struct {
 	// gorm.Model
-	Key       string    `gorm:"primarykey"` // key
-	UID       int       // 用户 ID
-	CreatedAt time.Time // 创建时间
-	UpdatedAt time.Time // 更新时间
+	Key       string    `gorm:"column:key; primarykey"` // key
+	UID       int       `gorm:"column:uid"`             // 用户 ID
+	CreatedAt time.Time `gorm:"column:created_at"`      // 创建时间
+	UpdatedAt time.Time `gorm:"column:updated_at"`      // 更新时间
 }
 
 // Users 用户资料
 type Users struct {
-	UID        int       `gorm:"primarykey"` // 用户 ID
-	VIPDueDate time.Time // VIP 到期时间
-	Name       string    // 用户暱称
-	CreatedAt  time.Time // 创建时间
-	UpdatedAt  time.Time // 更新时间
+	UID        int       `gorm:"column:uid; primarykey"` // 用户 ID
+	VIPDueDate time.Time `gorm:"column:vip_due_date"`    // VIP 到期时间
+	Name       string    `gorm:"column:name"`            // 用户暱称
+	CreatedAt  time.Time `gorm:"column:created_at"`      // 创建时间
+	UpdatedAt  time.Time `gorm:"column:updated_at"`      // 更新时间
 }
 
 // PlayURLCache 播放链接缓存
 type PlayURLCache struct {
-	ID         uint       `gorm:"primarykey"` // ...
-	IsVip      bool       // 大会员
-	CID        int        // ...
-	Area       Area       // 地区
-	DeviceType DeviceType // 装置种类
-	EpisodeID  int        // 剧集 ID
-	JSONData   string     // 内容
-	CreatedAt  time.Time  // 创建时间
-	UpdatedAt  time.Time  // 更新时间
+	ID         uint       `gorm:"column:id; primarykey"` // ...
+	IsVip      bool       `gorm:"column:is_vip"`         // 大会员
+	CID        int        `gorm:"column:cid"`            // cid
+	Area       Area       `gorm:"column:area"`           // 地区
+	DeviceType DeviceType `gorm:"column:device_type"`    // 装置种类
+	EpisodeID  int        `gorm:"column:episode_id"`     // 剧集 ID
+	JSONData   string     `gorm:"column:json_data"`      // 内容
+	CreatedAt  time.Time  `gorm:"column:created_at"`     // 创建时间
+	UpdatedAt  time.Time  `gorm:"column:updated_at"`     // 更新时间
 }
 
 // THSeasonCache season 缓存
 type THSeasonCache struct {
-	SeasonID  int       `gorm:"primarykey"` // ...
-	JSONData  string    // 内容
-	CreatedAt time.Time // 创建时间
-	UpdatedAt time.Time // 更新时间
+	SeasonID  int       `gorm:"column:season_id; primarykey"` // ...
+	JSONData  string    `gorm:"column:json_data"`             // 内容
+	CreatedAt time.Time `gorm:"column:created_at"`            // 创建时间
+	UpdatedAt time.Time `gorm:"column:updated_at"`            // 更新时间
 }
 
 // THSubtitleCache 字幕缓存
 type THSubtitleCache struct {
-	EpisodeID int       `gorm:"primarykey"` // ...
-	JSONData  string    // 内容
-	CreatedAt time.Time // 创建时间
-	UpdatedAt time.Time // 更新时间
+	EpisodeID int       `gorm:"column:episode_id; primarykey"` // ...
+	JSONData  string    `gorm:"column:json_data"`              // 内容
+	CreatedAt time.Time `gorm:"column:created_at"`             // 创建时间
+	UpdatedAt time.Time `gorm:"column:updated_at"`             // 更新时间
 }
 
 // History 历史记录(统计)

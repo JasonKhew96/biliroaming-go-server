5bf6bbac7f036523a1d127c75ffac5baf75bad84
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2022-07-12 15:28:00 +0800

    fix: database migration

--- sql/migrations/0007-fix-season-caches.sql
+++ sql/migrations/0007-fix-season-caches.sql
@@ -1,28 +1,66 @@
 -- +migrate Up
-DELETE FROM th_season_episode_caches;
-DELETE FROM th_season_caches;
-ALTER TABLE th_season_episode_caches DROP CONSTRAINT th_season_episode_caches_season_id_fkey;
-ALTER TABLE th_season_caches DROP CONSTRAINT th_season_caches_pkey;
-ALTER TABLE th_season_caches ADD COLUMN id SERIAL PRIMARY KEY;
+DROP TABLE IF EXIST th_season_episode_caches;
+DROP TABLE IF EXIST th_season_caches;
+DROP TABLE IF EXIST th_season2_episode_caches;
+DROP TABLE IF EXIST th_season2_caches;
 
-DELETE FROM th_season2_episode_caches;
-DELETE FROM th_season2_caches;
-ALTER TABLE th_season2_episode_caches DROP CONSTRAINT th_season2_episode_caches_season_id_fkey;
-ALTER TABLE th_season2_caches DROP CONSTRAINT th_season2_caches_pkey;
-ALTER TABLE th_season2_caches ADD COLUMN id SERIAL PRIMARY KEY;
-
+CREATE TABLE th_season_caches(
+    id SERIAL PRIMARY KEY,
+    season_id BIGINT NOT NULL,
+    is_vip BOOLEAN NOT NULL,
+    data JSON NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+CREATE TABLE th_season_episode_caches(
+    episode_id BIGINT PRIMARY KEY NOT NULL,
+    season_id BIGINT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+CREATE TABLE th_season2_caches(
+    id SERIAL PRIMARY KEY,
+    season_id BIGINT NOT NULL,
+    is_vip BOOLEAN NOT NULL,
+    data JSON NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+CREATE TABLE th_season2_episode_caches(
+    episode_id BIGINT PRIMARY KEY NOT NULL,
+    season_id BIGINT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
 
 -- +migrate Down
-DELETE FROM th_season_episode_caches;
-DELETE FROM th_season_caches;
-ALTER TABLE th_season_caches DROP CONSTRAINT th_season_caches_pkey;
-ALTER TABLE th_season_caches DROP COLUMN id;
-ALTER TABLE th_season_caches ADD PRIMARY KEY (season_id);
-ALTER TABLE th_season_episode_caches ADD CONSTRAINT th_season_episode_caches_season_id_fkey FOREIGN KEY (season_id) REFERENCES th_season_caches(season_id);
-
-DELETE FROM th_season2_episode_caches;
-DELETE FROM th_season2_caches;
-ALTER TABLE th_season2_caches DROP CONSTRAINT th_season2_caches_pkey;
-ALTER TABLE th_season2_caches DROP COLUMN id;
-ALTER TABLE th_season2_caches ADD PRIMARY KEY (season_id);
-ALTER TABLE th_season2_episode_caches ADD CONSTRAINT th_season2_episode_caches_season_id_fkey FOREIGN KEY (season_id) REFERENCES th_season2_caches(season_id);
+DROP TABLE IF EXIST th_season_episode_caches;
+DROP TABLE IF EXIST th_season_caches;
+DROP TABLE IF EXIST th_season2_episode_caches;
+DROP TABLE IF EXIST th_season2_caches;
+CREATE TABLE th_season_caches(
+    season_id BIGINT PRIMARY KEY NOT NULL,
+    is_vip BOOLEAN NOT NULL,
+    data JSON NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+CREATE TABLE th_season_episode_caches(
+    episode_id BIGINT PRIMARY KEY NOT NULL,
+    season_id REFERENCES th_season_caches(season_id) BIGINT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+CREATE TABLE th_season2_caches(
+    season_id BIGINT PRIMARY KEY NOT NULL,
+    is_vip BOOLEAN NOT NULL,
+    data JSON NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+CREATE TABLE th_season2_episode_caches(
+    episode_id BIGINT PRIMARY KEY NOT NULL,
+    season_id BIGINT REFERENCES th_season2_caches(season_id) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);

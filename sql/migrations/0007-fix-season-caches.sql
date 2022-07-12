eb221eae1a6932e465aafff5fabbcbaeabb4fa1f
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2022-07-12 16:44:14 +0800

    fix: database migration

--- sql/migrations/0007-fix-season-caches.sql
+++ sql/migrations/0007-fix-season-caches.sql
@@ -1,8 +1,8 @@
 -- +migrate Up
-DROP TABLE IF EXIST th_season_episode_caches;
-DROP TABLE IF EXIST th_season_caches;
-DROP TABLE IF EXIST th_season2_episode_caches;
-DROP TABLE IF EXIST th_season2_caches;
+DROP TABLE IF EXISTS th_season_episode_caches;
+DROP TABLE IF EXISTS th_season_caches;
+DROP TABLE IF EXISTS th_season2_episode_caches;
+DROP TABLE IF EXISTS th_season2_caches;
 
 CREATE TABLE th_season_caches(
     id SERIAL PRIMARY KEY,
@@ -34,10 +34,10 @@ CREATE TABLE th_season2_episode_caches(
 );
 
 -- +migrate Down
-DROP TABLE IF EXIST th_season_episode_caches;
-DROP TABLE IF EXIST th_season_caches;
-DROP TABLE IF EXIST th_season2_episode_caches;
-DROP TABLE IF EXIST th_season2_caches;
+DROP TABLE IF EXISTS th_season_episode_caches;
+DROP TABLE IF EXISTS th_season_caches;
+DROP TABLE IF EXISTS th_season2_episode_caches;
+DROP TABLE IF EXISTS th_season2_caches;
 CREATE TABLE th_season_caches(
     season_id BIGINT PRIMARY KEY NOT NULL,
     is_vip BOOLEAN NOT NULL,

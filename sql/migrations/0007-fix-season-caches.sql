007beb565a37b33041b4ab195b0629ae442b69de
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2022-07-12 12:30:31 +0800

    fix: sql migration

--- sql/migrations/0007-fix-season-caches.sql
+++ sql/migrations/0007-fix-season-caches.sql
@@ -5,8 +5,8 @@ ALTER TABLE th_season_episode_caches DROP CONSTRAINT th_season_episode_caches_se
 ALTER TABLE th_season_caches DROP CONSTRAINT th_season_caches_pkey;
 ALTER TABLE th_season_caches ADD COLUMN id SERIAL PRIMARY KEY;
 
-DELETE FROM th_season2_caches;
 DELETE FROM th_season2_episode_caches;
+DELETE FROM th_season2_caches;
 ALTER TABLE th_season2_episode_caches DROP CONSTRAINT th_season2_episode_caches_season_id_fkey;
 ALTER TABLE th_season2_caches DROP CONSTRAINT th_season2_caches_pkey;
 ALTER TABLE th_season2_caches ADD COLUMN id SERIAL PRIMARY KEY;
@@ -20,8 +20,8 @@ ALTER TABLE th_season_caches DROP COLUMN id;
 ALTER TABLE th_season_caches ADD PRIMARY KEY (season_id);
 ALTER TABLE th_season_episode_caches ADD CONSTRAINT th_season_episode_caches_season_id_fkey FOREIGN KEY (season_id) REFERENCES th_season_caches(season_id);
 
-DELETE FROM th_season2_caches;
 DELETE FROM th_season2_episode_caches;
+DELETE FROM th_season2_caches;
 ALTER TABLE th_season2_caches DROP CONSTRAINT th_season2_caches_pkey;
 ALTER TABLE th_season2_caches DROP COLUMN id;
 ALTER TABLE th_season2_caches ADD PRIMARY KEY (season_id);
